9f680ec53e485fad5435046240c94c2fb142cf49
lin <100304367+longstorycourt@users.noreply.github.com>
2024-05-30 15:06:48 +0700

    create front page

--- MiniGames/src/minigames/MainFrame.java
+++ MiniGames/src/minigames/MainFrame.java
@@ -1,16 +1,60 @@
 package minigames;
 import java.awt.Color;
-import java.awt.Frame;
+import java.awt.Font;
+import java.awt.FlowLayout;
+import java.awt.GridLayout;
+import java.awt.BorderLayout;
 
-public class MainFrame extends Frame {
+import javax.swing.JFrame;
+import javax.swing.JTextField;
+import javax.swing.JPasswordField;
+import javax.swing.JButton;
+// import javax.swing.ImageIcon;
+import javax.swing.JPanel;
+import javax.swing.JLabel;
+
+public class MainFrame extends JFrame {
+    // ImageIcon icon = new ImageIcon("video-game-gamepad-icon.png");
+    // JLabel lblIcon = new JLabel(icon);
+    JLabel lblTitle = new JLabel("Welcome to our Homepage!");
+    JLabel lblSubtitle = new JLabel("Many fun games await!!!");
+    JLabel lblLogin = new JLabel("Log in to start your journey!");
+    JLabel lblUser = new JLabel("Username: ");
+    JLabel lblPw = new JLabel("Password: ");
+
+    JTextField TextUser = new JTextField("");
+    JPasswordField TextPw = new JPasswordField();
+
+    JButton btnLogin = new JButton("Log in");
+    JButton btnQuit = new JButton("Quit");
+
+    JPanel pnlHello = new JPanel(new FlowLayout());
+    JPanel pnlLogin = new JPanel(new BorderLayout());
+    JPanel pnlLogin1 = new JPanel(new GridLayout(3,2));
+
     public MainFrame(){
-        super("Mini Games");
+        lblTitle.setFont(new Font("cursive",Font.BOLD,25));
+        //add(lblIcon);
+        pnlHello.add(lblTitle);
+        pnlHello.add(lblSubtitle);
+
+        pnlLogin.add(lblLogin,BorderLayout.NORTH);
+        pnlLogin1.add(lblUser);        pnlLogin1.add(TextUser);
+        pnlLogin1.add(lblPw);          pnlLogin1.add(TextPw);
+        pnlLogin1.add(btnLogin);       pnlLogin1.add(btnQuit);  
+        pnlLogin.add(pnlLogin1, BorderLayout.CENTER);
+
+        add(pnlHello,BorderLayout.NORTH);
+        add(pnlLogin,BorderLayout.CENTER);
+
+        TextUser.setFocusable(true);
+
+        setTitle("Mini Games");
         setSize(450,450);
-        setLayout(null);
-        setResizable(false);
+        setLayout(new BorderLayout());
+        setResizable(true);
         setLocationRelativeTo(null);
-        setVisible(true);
-        setBackground(new Color(255, 204, 102));
+        setBackground(new Color(255, 204, 102));setVisible(true);
     }
     public static void main(String[] args) {
         new MainFrame();
